0f6775cf85d0b6c44a215445e79d3525e0c61615
Simon Ngo <Simonhunghcd@gmail.com>
2025-09-22 22:59:35 -0400

    Shown option to register new card, then store it in a json file.

--- main.py
+++ main.py
@@ -9,13 +9,11 @@ import time
 
 # Student information UID : Name (Store this in some file in the future)
 # I will change the method later so that we can have an option to register new people if they scan for first time
-STUDENTS = { 
-    "6B 94 70 56 00" : "Simon"
-} 
-l_status = {} 
-
+RM_ER = "RM_ers.json"
 r = readers() 
 reader = r[0]
+l_status = {}
+
 
 def get_uid(connection): #Get student UID  
     GET_UID = [0xFF, 0xCA, 0x00, 0x00, 0x00]
@@ -48,14 +46,36 @@ def write_csv_attendance(name,c_status):
         w.writerow([name,c_status,now_time,now_date])
     print(f"LOG: {name} - {c_status} - {now_time} - {now_date} ")
 
+def load_uid(): #Load json file that have students information
+    if os.path.exists(RM_ER):
+        with open(RM_ER,"r") as f:
+            return json.load(f)
+    return {}
+
+def save_uid(students): #save new student into json file
+    with open(RM_ER, "w") as f:
+        json.dump(students, f, indent=4) #New info learn 1 tab = 4 spaces :)))
+
 def main():
     connection = reader.createConnection() # create connection
+    students = load_uid()
     while True:
         try:
             connection.connect()
             uid = get_uid(connection)
             if uid:
-                name = STUDENTS.get(uid, "Unknown card")
+                if uid not in students:
+                    print(f"Unknown card detected: {uid}")
+                    choice = input("Do you want to register this card? (y/n): ").strip().lower()
+                    if choice == ("y" or "Y"):
+                        name = input("Enter student name: ").strip()
+                        students[uid] = name
+                        save_uid(students)
+                        print(f"Registered {name} with UID {uid}! WELCOME TO RPI MOTORSPORTS !!!")
+                    else:
+                        print("Ignored action!")
+                        continue
+                name = students[uid]
                 print(name)
                 c_status = "IN" 
                 if l_status.get(uid) == "IN":
